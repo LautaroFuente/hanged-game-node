bdfab4522d7bcb09ca3d4c8680f1aab7c42ee736
LautaroFuente <lautaro.fuente@yahoo.com>
2024-06-06 02:38:20 -0300

    .

--- models/database.js
+++ models/database.js
@@ -3,28 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-/*const connection = await mysql.createConnection({
+const connection = await mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT || 3306,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});*/
-
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
-/*try {
+try {
   await connection.connect();
   console.log("Se establecio conexion");
 } catch (err) {
   console.error("Hubo un error al establecer conexion:", err);
-}*/
+}
 
 export default connection;
